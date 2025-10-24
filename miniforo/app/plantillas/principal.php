c164bb79cea5c0620d6aa2f1a0ff666145c43ceb
Parsa5118 <parsanoroo31@gmail.com>
2025-10-24 19:31:46 +0200

    Add files via upload

---
+++ miniforo/app/plantillas/principal.php
@@ -0,0 +1,22 @@
+
+
+<!DOCTYPE html>
+<head>
+<meta charset="UTF-8">
+<link href="web/default.css" rel="stylesheet" type="text/css" />
+<title>MINIFORO</title>
+</head>
+<body>
+<div id="container" style="width: 450px;">
+<div id="header">
+<h1>MINIFORO versión 1.0</h1>
+</div>
+
+<div id="content">
+   <?=$contenido_php ?>
+</div>
+</div>
+</body>
+</html>
+
+
